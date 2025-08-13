ca6a31f29c4e0f8c814bb6be269abf8372018787
valencullari <valentinocullari2007@gmail.com>
2025-08-13 18:19:04 -0300

    Update (sin terminar) PanelPrincipal.java

--- Juego/src/Clases/PanelPrincipal.java
+++ Juego/src/Clases/PanelPrincipal.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.imageio.ImageIO;
 import java.util.Random;
+import javax.swing.JButton;
 
 public class PanelPrincipal extends JPanel {
 
@@ -23,6 +24,17 @@ public class PanelPrincipal extends JPanel {
     private int y2;
     private int margen;
     private int tamanoletra;
+    private int rojoc;
+    private int azulc;
+
+    private BufferedImage fondorojo1;
+    private BufferedImage fondoAzul1;
+    private BufferedImage fondoAzul20;
+    private BufferedImage fondoAzul21;
+    private BufferedImage fondorojo20;
+    private BufferedImage fondorojo21;
+    private BufferedImage fondoempate1;
+    
 
     private int pelotaX, pelotaY;
     private int pelotaSize;
@@ -30,9 +42,13 @@ public class PanelPrincipal extends JPanel {
     private int pelotaVelY = 10;
     private int golesIzquierda = 0;
     private int golesDerecha = 0;
+    private String mensajeGanador = null;
+    private boolean juegoTerminado = false;
+    private JButton botonReiniciar;
+    
 
 
-    private int segundosTotales = 90; 
+    private int segundosTotales = 3; 
     private Timer timerJuego;         
     private Timer timerCuenta;       
 
@@ -45,7 +61,15 @@ public class PanelPrincipal extends JPanel {
 
     public PanelPrincipal() {
         setFocusable(true);
+       
+        botonReiniciar = new JButton("Volver a jugar");
+        botonReiniciar.setVisible(false);
+        botonReiniciar.addActionListener(e -> reiniciarJuego());
+        this.add(botonReiniciar);
+        
+        
 
+        //Carga de texturas
         try { fondo = ImageIO.read(getClass().getResource("/imagenes/fondo.jpg")); } 
         catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
 
@@ -58,6 +82,28 @@ public class PanelPrincipal extends JPanel {
         try { paletaRoja = ImageIO.read(getClass().getResource("/imagenes/paleta roja.jpeg")); } 
         catch (IOException e) { System.out.println("No se pudo cargar la imagen de la paleta roja"); }
 
+        try { fondoAzul1 = ImageIO.read(getClass().getResource("/imagenes/0-1.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondoAzul20 = ImageIO.read(getClass().getResource("/imagenes/0-2.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondoAzul21 = ImageIO.read(getClass().getResource("/imagenes/1-2.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondorojo1 = ImageIO.read(getClass().getResource("/imagenes/1-0.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondorojo20 = ImageIO.read(getClass().getResource("/imagenes/2-0.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondorojo21 = ImageIO.read(getClass().getResource("/imagenes/2-1.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        try { fondoempate1 = ImageIO.read(getClass().getResource("/imagenes/1-1.jpeg")); } 
+        catch (IOException e) { System.out.println("No se pudo cargar la imagen de fondo"); }
+        
+        
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -120,26 +166,78 @@ public class PanelPrincipal extends JPanel {
 
             repaint();
         });
+        
         timerJuego.start();
+        
+        
 
 
         timerCuenta = new Timer(1000, e -> {
             segundosTotales--;
-            if (segundosTotales <= 0) {
+            
+            
+          if (segundosTotales <= 0) {
                 segundosTotales = 0;
 
                 timerJuego.stop();
                 timerCuenta.stop();
+                if (golesIzquierda > golesDerecha) {
+                    rojoc++;
+                    if (rojoc == 1 && azulc == 0) {
+                        fondo = fondorojo1;
+                        mensajeGanador = "ROJO PUNTO";
+                    } else if (rojoc == 2 && azulc == 0) {
+                        fondo = fondorojo20;
+                        mensajeGanador = "ROJO GANA";
+                        terminarJuegoPosta(); 
+                       
+                    } else if (rojoc == 2 && azulc == 1) {
+                        fondo = fondorojo21;
+                        mensajeGanador = "ROJO GANA";
+                        terminarJuegoPosta();
+                        
+                    } else if (rojoc == 1 && azulc == 1) {
+                        fondo = fondoempate1;
+                        mensajeGanador = "ROJO PUNTO";
+                    }
+                    
+                } else if (golesDerecha > golesIzquierda) {
+                    azulc++;
+                    if (azulc == 1 && rojoc == 0) {
+                        fondo = fondoAzul1;
+                        mensajeGanador = "AZUL PUNTO";
+                    } else if (azulc == 2 && rojoc == 0) {
+                        fondo = fondoAzul20;
+                        mensajeGanador = "AZUL GANA";
+                        terminarJuegoPosta();
+                        
+                        
+                    } else if (azulc == 2 && rojoc == 1) {
+                        fondo = fondoAzul21;
+                        mensajeGanador = "AZUL GANA";
+                        terminarJuegoPosta();
+                    } else if (azulc == 1 && rojoc == 1) {
+                        fondo = fondoempate1;
+                        mensajeGanador = "AZUL PUNTO";
+                    }
+                } else {
+                    mensajeGanador = "EMPATE";
+                }
 
-
+                if (juegoTerminado) {
+                	 new Timer(2000, ev ->{
+                		 
+                	 });
+                }
                 new Timer(2000, ev -> {
-                    segundosTotales = 90; 
+                    segundosTotales = 10; 
                     golesIzquierda = 0;
                     golesDerecha = 0;
+                    mensajeGanador = null;
                     posicionarPelotaCentro();
                     posicionarPaletasCentro();
 
-                    // Reiniciar timers
+
                     timerJuego.start();
                     timerCuenta.start();
                 }) {{
@@ -151,6 +249,25 @@ public class PanelPrincipal extends JPanel {
         timerCuenta.start();
     }
 
+    private void reiniciarJuego() {
+        juegoTerminado = false;
+        rojoc = 0;
+        azulc = 0;
+        golesIzquierda = 0;
+        golesDerecha = 0;
+        mensajeGanador = "";
+        botonReiniciar.setVisible(false);
+        paletasCentradas = false;
+        timerCuenta.start();
+    }
+    private void terminarJuegoPosta() {
+        juegoTerminado = true;
+        timerJuego.stop();
+        timerCuenta.stop();
+        botonReiniciar.setVisible(true);
+        
+    }
+    
     private void clampPositions() {
         int panelHeight = getHeight();
         int topMargin = (int) (panelHeight * 0.134);
@@ -158,7 +275,11 @@ public class PanelPrincipal extends JPanel {
         y1 = Math.max(topMargin, Math.min(y1, panelHeight - rectHeight));
         y2 = Math.max(topMargin, Math.min(y2, panelHeight - rectHeight));
     }
-
+    private void terminarJuego() {
+        timerJuego.stop();
+        timerCuenta.stop();
+      
+    }
     private void posicionarPaletasCentro() {
         int panelHeight = getHeight();
         y1 = (panelHeight - rectHeight) / 2;
@@ -171,6 +292,7 @@ public class PanelPrincipal extends JPanel {
     }
 
     private String formatearTiempo(int segundos) {
+    	
         int minutos = segundos / 60;
         int seg = segundos % 60;
         return String.format("%02d:%02d", minutos, seg);
@@ -185,6 +307,12 @@ public class PanelPrincipal extends JPanel {
 
         if (fondo != null) {
             g.drawImage(fondo, 0, 0, panelWidth, panelHeight, null);
+        }
+        if (mensajeGanador != null) {
+            g.setFont(new java.awt.Font("pixeled", java.awt.Font.BOLD, (int) (panelHeight * 0.1)));
+            int anchoTextoGanador = g.getFontMetrics().stringWidth(mensajeGanador);
+            g.setColor(java.awt.Color.YELLOW);
+            g.drawString(mensajeGanador, (panelWidth - anchoTextoGanador+5) / 2, panelHeight / 2);
         }
 
         rectWidth = (int) (panelWidth * 0.02);
@@ -203,9 +331,9 @@ public class PanelPrincipal extends JPanel {
         g.setColor(java.awt.Color.WHITE);
         g.setFont(new java.awt.Font("pixeled", java.awt.Font.BOLD, tamanoletra = (int) (panelHeight * 0.03)));
 
-        // Marcador
-        margen = (int) (panelHeight * 0.045);
-        g.drawString(Integer.toString(golesIzquierda), margen, (int) (panelHeight * 0.09));
+        // Resultado
+        margen = (int) (panelHeight * 0.066);
+        g.drawString(Integer.toString(golesIzquierda), margen+20 , (int) (panelHeight * 0.09));
         g.drawString(Integer.toString(golesDerecha), panelWidth - margen - 40, (int) (panelHeight * 0.09));
 
         // Temporizador
@@ -228,6 +356,3 @@ public class PanelPrincipal extends JPanel {
         }
     }
 }
-
-
-
